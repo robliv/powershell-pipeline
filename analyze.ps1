84e2dfa553353d24880bd98827383f00a6058252
robliv <46368581+robliv@users.noreply.github.com>
2020-02-21 16:10:35 +0200

    fix

--- analyze.ps1
+++ analyze.ps1
@@ -5,19 +5,15 @@ try {
     Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 –Force
     Install-Module -Name PSScriptAnalyzer
 
-    $analysis = Invoke-ScriptAnalyzer -Path .\$($env:ModuleName) -Settings PSGallery -Recurse
+    $analysis = Invoke-ScriptAnalyzer -Path ".\$($env:ModuleName)" -Settings PSGallery -Recurse
 
     $analysis
 
     if ($analysis){
         $analysis
-        Write-Output "Failed static code analysis!"
-        Exit 12345
     }
 
 }
 catch {
     $error
-    write-output "Critical error in script. Exiting."
-    $error.Clear()
 }
