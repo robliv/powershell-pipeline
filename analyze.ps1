d4a582d75365008007b2283ab422d15e7d1bd71a
robliv <46368581+robliv@users.noreply.github.com>
2020-02-21 16:13:11 +0200

    fix

--- analyze.ps1
+++ analyze.ps1
@@ -2,18 +2,20 @@ write-output "Running static tests!"
 $ErrorActionPreference = "stop"
 
 try {
-    Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 –Force
+    Install-PackageProvider Nuget -MinimumVersion 2.8.5.201 -Force
     Install-Module -Name PSScriptAnalyzer
 
     $analysis = Invoke-ScriptAnalyzer -Path ".\$($env:ModuleName)" -Settings PSGallery -Recurse
 
-    $analysis
 
     if ($analysis){
         $analysis
+        throw "Failed static code analysis!"
     }
 
 }
 catch {
     $error
+    write-output "Critical error in script. Exiting."
+    $error.Clear()
 }
